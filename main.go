e62ad97b45a5432a49d8072de83aeca5068c72cc
Michael Maximilien <maxim@us.ibm.com>
2016-07-22 18:31:58 -0700

    using VCAP_SERVICES
    Signed-off-by: Nima Kaviani <nkavian@us.ibm.com>

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"net/http"
 	"os"
+	"regexp"
 	"syscall"
 
 	"github.com/nimakaviani/gomeeting-broker/handlers"
@@ -21,26 +23,49 @@ var port = flag.String(
 	"Server port",
 )
 
+var sqlConnectionRegex = regexp.MustCompile(`^.+:\/\/(.+):(.+)@(.+):\d+\/(.+)$`)
+
+type Credentials struct {
+	URI string `json:"uri,omitempty"`
+}
+
+type ElephantSql struct {
+	Credentials Credentials `json:"credentials,omitempty"`
+}
+
+type VCAPServices struct {
+	Elephantsql []ElephantSql `json:"elephantsql,omitempty"`
+}
+
+type VCAPServicesStruct struct {
+	VCAP_SERVICES VCAPServices `json:"VCAP_SERVICES"`
+}
+
+type DBCredentials struct {
+	user     string
+	password string
+	host     string
+	name     string
+}
+
 func main() {
 	flag.Parse()
 	httpServer := http_server.New(":"+*port, http.DefaultServeMux)
 
-	println("reading config ...")
 	config, err := models.LoadConfig("config.json")
 	if err != nil {
 		panic(err)
 	}
 
-	println("initializing datastore ...")
-	user, password, name, host := os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_HOST")
-	datastore := utils.NewDBDataStore(user, password, name, host)
+	dbCreds := getDBCreds()
+
+	datastore := utils.NewDBDataStore(dbCreds.user, dbCreds.password, dbCreds.name, dbCreds.host)
 	err = datastore.Init()
 	if err != nil {
 		panic(err)
 	}
 	defer datastore.Close()
 
-	println("initializing handlers ...")
 	handler := handlers.NewHandler(config, datastore)
 	http.HandleFunc("/findroom", handler.Alexa)
 	http.HandleFunc("/google73d91fa1cfb6fa88.html", handler.Verify)
@@ -57,5 +82,38 @@ func main() {
 	err = <-monitor.Wait()
 	if err != nil {
 		panic(err)
+	}
+}
+
+func getDBCreds() DBCredentials {
+	vcapServicesString := os.Getenv("VCAP_SERVICES")
+
+	if vcapServicesString == "" {
+		vcapServicesString = "{}"
+	}
+
+	vcapServices := VCAPServicesStruct{}
+
+	err := json.Unmarshal([]byte(vcapServicesString), &vcapServices)
+	if err != nil {
+		panic(err)
+	}
+
+	if len(vcapServices.VCAP_SERVICES.Elephantsql) == 0 {
+		return DBCredentials{
+			user:     os.Getenv("DB_USER"),
+			password: os.Getenv("DB_PASSWORD"),
+			host:     os.Getenv("DB_HOST"),
+			name:     os.Getenv("DB_NAME"),
+		}
+	}
+
+	matches := sqlConnectionRegex.FindStringSubmatch(vcapServices.VCAP_SERVICES.Elephantsql[0].Credentials.URI)
+
+	return DBCredentials{
+		user:     matches[1],
+		password: matches[2],
+		host:     matches[3],
+		name:     matches[4],
 	}
 }
